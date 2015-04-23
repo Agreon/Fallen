7c63f35e20899ba65042b3cd326947e57687f1ab
Daniel Huth <dannyfore@web.de>
2015-04-23 11:12:07 +0200

    initial commit

---
+++ AgreonLibrary2/AL.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   AL.h
+ * Author: daniel
+ *
+ * Created on February 2, 2015, 5:00 PM
+ */
+
+#ifndef AL_H
+#define	AL_H
+
+#include "Graphics.h"
+#include "Animation.h"
+#include "Input.h"
+#include "Sound.h"
+
+#endif	/* AL_H */
+
